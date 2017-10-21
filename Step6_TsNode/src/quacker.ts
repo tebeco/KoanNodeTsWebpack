dfdda4b94b9ce0e4d53bd7d4acf195340f715b32
TeBeCo <tebeco@gmail.com>
2017-10-21 16:03:25 +0200

    step6: using TsNode

---
+++ Step6_TsNode/src/quacker.ts
@@ -0,0 +1,11 @@
+require('ts-node').register()
+
+export interface IQuack {
+    please(): string;
+}
+
+export function ducker() : IQuack {
+    return {
+        please: () => "QuackQuack QuackQuackQuack !!"
+    }
+}
